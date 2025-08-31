cc84677f0a6338e7e14ed57bbaadef35c98b538b
yusuke kusano <yuyuyu@example.com>
2025-09-01 00:51:22 +0900

    v65 js/generator.js Update

--- js/generator.js
+++ js/generator.js
@@ -1,7 +1,21 @@
 // js/generator.js
+// @ts-check
+
+/** @typedef {import('./types').Model} Model */
+
 import { buildModel } from './model.js';
 import { render, fixBottomSync } from './renderer.js';
 
+/**
+ * @typedef {Object} GenerateDeps
+ * @property {(mode:string)=>void} [setZoom]
+ */
+
+/**
+ * 生成パイプライン本体
+ * @param {{ csvText: string, zoomMode?: string } & GenerateDeps} args
+ */
+ 
 export function generateCore({ csvText, zoomMode, setZoom }) {
   try {
     buildModel(csvText || '');
